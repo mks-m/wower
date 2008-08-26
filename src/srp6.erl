5fe65949f40fc6c1111c9f0a65617701124c7b2a
keymone <keymone@gmail.com>
2008-08-26 20:13:21 +0300

    one day there will be war because of endianness >:)

--- src/srp6.erl
+++ src/srp6.erl
@@ -2,6 +2,7 @@
 -export([challenge/1, proof/2, test/0]).
 
 -define(IN, /unsigned-little-integer).
+-define(NI, /unsigned-big-integer).
 -define(QQ, :256?IN).
 -define(SH, :160?IN).
 -define(DQ, :128?IN).
@@ -16,7 +17,8 @@ challenge(Credentials) ->
     Generator = 7,
     Salt      = 16#B4A2BA1BECF0034B869FA1BE8460C73C69C84FAF43710A1F0700D7E68F4531EB, % will be random
     SecretB   = 16#73CA17CFC1E4EA1A30A169D3BF471C0962622785818C4FCE903128D82258BE25, % will be random
-    <<X?SH>>  = crypto:sha(<<Salt?QQ, (Credentials)?b>>),
+    X         = sha(<<Salt:256?NI, Credentials:160?NI>>),
+    io:format("X = ~.16B~n", [X]),
     Verifier  = crypto:mod_exp(Generator, X, Modulus),
     PublicB   = crypto:mod_exp(Verifier * 3 + crypto:mod_exp(Generator, SecretB, Modulus), 1, Modulus),
     {Modulus, Generator, PublicB, SecretB, Verifier, Salt}.
@@ -24,8 +26,14 @@ challenge(Credentials) ->
 proof(PublicA, MProof1) ->
     ok.
 
+sha(Data) ->
+    <<Result:160/big>> = crypto:sha(Data),
+    Result.
+
 test() ->
     User = "WCELL",
     Pass = "RULES",
-    Hash = crypto:sha(User ++ ":" ++ Pass),
-    {N, G, PB, SC, V, S} = challenge(Hash).
+    Credentials = list_to_binary(User ++ ":" ++ Pass),
+    Hash        = sha(Credentials),
+    io:format("C: ~.16B~n", [Hash]),
+    {N, G, PB, SB, V, S} = challenge(Hash).

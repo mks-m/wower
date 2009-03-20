0155b2e40a8734f128e8e932efd60da301719b9f
SPY <spy_forever@pochta.ru>
2009-03-21 01:49:26 +0300

    Add cmsg_logout_query and cmsg_logout_cancle handlers

--- realm/src/handlers_misc.erl
+++ realm/src/handlers_misc.erl
@@ -9,3 +9,14 @@ cmsg_ping(S, State, Data) ->
     S ! {self(), smsg_pong, <<Sequence?L>>},
     State#client_state{latency=Latency}.
 	
+cmsg_logout_request(S, State, _Data) ->
+	S ! {self(), smsg_logout_response, <<0?L?IN, 0?B?IN>>},
+	State#client_state{logout=realm_patterns:cmsg_logout_request(S, self())}.
+	
+cmsg_logout_cancel(_S, State, _Data) when State#client_state.logout =:= no ->
+	State;
+
+cmsg_logout_cancel(_S, State, _Data) ->
+	Logout = State#client_state.logout,
+	Logout ! {self(), exit},
+	State#client_state{logout = no}.

da98f44b923d8587283a22ccf3f2a1f921eafed9
SPY <spy_forever@pochta.ru>
2009-03-21 12:26:24 +0300

    Rewrite cmsg_logout_cancel handler

--- realm/src/handlers_misc.erl
+++ realm/src/handlers_misc.erl
@@ -12,11 +12,11 @@ cmsg_ping(S, State, Data) ->
 cmsg_logout_request(S, State, _Data) ->
 	S ! {self(), smsg_logout_response, <<0?L?IN, 0?B?IN>>},
 	State#client_state{logout=realm_patterns:cmsg_logout_request(S, self())}.
-	
-cmsg_logout_cancel(_S, State, _Data) when State#client_state.logout =:= no ->
-	State;
 
-cmsg_logout_cancel(_S, State, _Data) ->
+cmsg_logout_cancel(_S, State, _Data) when pid(State#client_state.logout) ->
 	Logout = State#client_state.logout,
 	Logout ! {self(), exit},
-	State#client_state{logout = no}.
+	State#client_state{logout = no};
+	
+cmsg_logout_cancel(_S, State, _Data) ->
+	State.

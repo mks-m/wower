a28e0e47410098b78a3c7c64c0d4fd7acb2da7e8
keymone <keymone@gmail.com>
2009-04-11 13:48:40 +0300

    account_helper is more like common thing.. added update.hrl into repo

---
+++ realm/include/update.hrl
@@ -0,0 +1,12 @@
+-record(update_block, {update_type,
+                       object_guid,
+                       object_type,
+                       update_flags,
+                       movement_flags,
+                       unknown = 0,
+                       game_time,
+                       position,
+                       fall_time,
+                       speeds,
+                       mask,
+                       fields}).
